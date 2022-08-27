f7c7dbbe78d28c765ddce4f6a2e31366c8a6ae7e
ramirezmz <roberto.paolo@dremio.com>
2022-08-27 19:37:05 -0300

    practice

---
+++ index.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3]
+
+listSum = sum(my_list)
+print(f"Sum of list = {listSum}")
+
+
+myList2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+listSum = 0
+
+for number in myList2:
+    listSum += number
+
+print(f"Sum of list -> {listSum}")
